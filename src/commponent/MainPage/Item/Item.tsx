c4c9fa98764a95b951452aeec23f257a54eaf945
Russen <russen333@gmail.com>
2023-06-14 18:33:23 +0300

    finished card

--- src/commponent/MainPage/Item/Item.tsx
+++ src/commponent/MainPage/Item/Item.tsx
@@ -2,13 +2,13 @@ import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import { Badge, Box, Button, CardActionArea, IconButton, Rating, Typography, keyframes } from '@mui/material';
+import { Badge, Box, Button, CardActionArea, IconButton, Paper, Rating, Typography, keyframes } from '@mui/material';
 import { itemDate } from './itemDate';
 import { IItem } from '../../../Models/item/itemModel';
 import Banner from './banner';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
-import Grid from '@mui/material/Unstable_Grid2/Grid2';
+
 
 
 const scale = keyframes`
@@ -19,18 +19,22 @@ const scale = keyframes`
   100% {
    transform: scale(1);
   }
-`
+`;
 
 
 const Item = () => {
+	//=====================
+	const item: IItem = JSON.parse(itemDate);
 
-	const date: IItem = JSON.parse(itemDate);
-	date.banners = ['hit', 'promotion'];
-	date.oldPreise = 101.23
+	item.banners = ['hit', 'promotion'];
+	item.oldPreise = 101.23
+	//==================================
 
-	console.log(date);
 
-	const banners = date.banners.map((e, i) => <Banner text={e} key={e + i} />);
+	const addCurrency = (item: string | number, currency = '$'): string | null => item ? `${item} ${currency}` : null;
+
+	const banners: JSX.Element[] | null = item.banners && item.banners.length > 0 ? item.banners.map((e, i) => <Banner text={e} key={e + i} />) : null;
+
 
 
 	const onClickImg = () => {
@@ -47,6 +51,10 @@ const Item = () => {
 		console.log('Count');
 	};
 
+	const onClickAddToCart = () => {
+		console.log('to cart');
+	};
+
 
 
 
@@ -59,8 +67,8 @@ const Item = () => {
 				<CardMedia
 					component="img"
 					height="298"
-					image={date.image}
-					alt={date.title}
+					image={item.image}
+					alt={item.title}
 					sx={{ mb: 2 }}
 				/>
 
@@ -109,32 +117,58 @@ const Item = () => {
 					display: 'flex',
 					alignItems: 'center',
 				}}>
-					<Rating name="half-rating" defaultValue={+date.rating.rate} precision={0.5} readOnly />
+					<Rating name="half-rating" defaultValue={+item.rating.rate} precision={0.5} readOnly />
 
 					<IconButton onClick={onclickCount}>
-						<Badge badgeContent={date.rating.count} color='info' >
+						<Badge badgeContent={item.rating.count} color='info' >
 							<ChatBubbleOutlineIcon />
 						</Badge>
 					</IconButton>
 				</Box>
 
-				<Typography sx={[(thema) => ({
+				<Typography sx={[{ mb: 2 }, (thema) => ({
 					color: thema.colors.black,
 					'.item-card:hover &': { color: thema.colors.blue2 },
 				})]}>
-					{date.title}
+					{item.title}
 				</Typography>
-				{/* <Grid container spacing={2}>
-					<Grid xs={6}>
-						1
-					</Grid>
 
-					<Grid xs={6}>
-						2
-					</Grid>
+				<Box sx={{
+					width: '100%',
+					display: 'grid',
+					gridTemplateColumns: '1fr 1fr',
+					gridTemplateRows: '36px 24px',
+					alignItems: 'center',
+					gap: '5px',
+				}}>
 
-				</Grid> */}
 
+					<Typography sx={{
+						fontWeight: '700',
+						fontSize: '24px',
+
+					}}>{addCurrency(item.price)}</Typography>
+
+					<Button variant="outlined" className='item' onClick={onClickAddToCart}
+						sx={
+							[(thema) => ({
+								'.item-card:hover &': {
+									backgroundColor: thema.colors.blue2,
+									color: thema.colors.white
+								},
+							})]
+
+						}>ADD TO CART</Button>
+
+					<Typography sx={[{
+						fontWeight: '700',
+						fontSize: '18px',
+
+						textDecoration: 'line-through',
+					}, thema => ({ color: thema.colors.red })]}>
+						{addCurrency(item.oldPreise)}</Typography>
+
+				</Box>
 
 			</CardContent>
 
